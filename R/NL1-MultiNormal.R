0c92fb54e6bc47b043f3ce034058ab91f73b964c
Niels Keizer <n.a.keizer@gmail.com>
2016-09-17 17:00:23 +0200

    Kleine aanpassing in R code voor vraag 18.

--- R/NL1-MultiNormal.R
+++ R/NL1-MultiNormal.R
@@ -188,6 +188,7 @@ Z_prime <- Z/chi5
 V_prime <- rowSums(Z_prime)
 d <- quantile(V_prime,0.975)
 stoploss_premium <- mean(pmax(V_prime-d,0)) 
+stoploss_premium
 
 # ---- Q19 ----
 #TODO: Nog helemaal uitwerken.
